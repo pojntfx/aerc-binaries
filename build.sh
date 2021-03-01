a52ed776c690dfd62650d5a7dbd34a1ed84c532d
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-03-01 14:15:40 +0000

    feat: Add basic build configuration

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install dependencies
+apt update
+apt install -y curl make git build-essential scdoc
+
+# Install Go
+GO_VERSION=1.16
+if [ "$(uname -m)" = 'x86_64' ]; then
+    curl -L -o /tmp/go.tar.gz https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
+else
+    curl -L -o /tmp/go.tar.gz https://golang.org/dl/go${GO_VERSION}.linux-arm64.tar.gz
+fi
+tar -C /usr/local -xzf /tmp/go.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Clone upstream
+rm -rf /data/upstream
+mkdir -p /data/upstream
+git clone https://git.sr.ht/~sircmpwn/aerc /data/upstream
+
+# Checkout latest release
+cd /data/upstream
+git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
+
+# Build upstream
+make aerc
+
+# Copy to staging directory
+mkdir -p /data/staging
+cp ./aerc /data/staging/aerc-linux.$(uname -m)
